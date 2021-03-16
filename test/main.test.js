38f13d13bdac32ad8c57a1b771fd93f508eab549
Kien Tran <kien.tran1995@gmail.com>
2021-03-15 17:25:38 -0700

    Adding more test cases for updating list

--- test/main.test.js
+++ test/main.test.js
@@ -1,9 +1,12 @@
 const fs = require("fs");
-const {parseHTML} = require('../tools_utils.js');
+const {parseHTML, updateListsOfURLs} = require('../tools_utils.js');
 var expect  = require('chai').expect;
 
 const testsCase = ["./test/htmlTest1.html", "./test/htmlTest2.html",
 "./test/htmlTest3.html"];
+
+const urlTestCase =["https://testUnique.com","https://testSimilar.com",
+"https://testSimilar.com"];
 
 
 describe("Testing Parser", function(){
@@ -26,7 +29,7 @@ describe("Testing Parser", function(){
             let arrayofURL = parseHTML(data);
             expect(arrayofURL.length).to.be.equal(0);
         });
-    })
+    });
     it("Should return 2 found links", function(){
         fs.readFile(testsCase[2], "utf8", (err, data) =>{
             if(err){
@@ -36,5 +39,23 @@ describe("Testing Parser", function(){
             let arrayofURL = parseHTML(data);
             expect(arrayofURL.length).to.be.equal(2);
         });
-    })
+    });
+});
+
+describe("Update List of URLs Test", function(){
+    var knownList = [];
+    describe("Known list initilized as empty", function(){
+        it("Should only remove duplicate in test cases", function(){
+            let output = updateListsOfURLs(knownList, urlTestCase);
+            expect(output.length).to.be.equal(2);
+            expect(output[0]).to.be.equal("https://testUnique.com");
+            expect(output[1]).to.be.equal("https://testSimilar.com");
+        });
+    });
+    describe("Second iteration through function", function(){
+        it("Should not output any more unique addresses", function(){
+            let output = updateListsOfURLs(knownList, urlTestCase);
+            expect(output.length).to.be.equal(0);
+        });
+    });
 });
