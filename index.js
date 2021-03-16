5aca5b3746b1a0ad29d849ef12da44449a1db4ce
Kien Tran <kien.tran1995@gmail.com>
2021-03-16 03:39:19 -0700

    Adding opions into start script. Reducing 5 to 3 in terms of depth max

--- index.js
+++ index.js
@@ -33,14 +33,14 @@ function closeEverything(){
  */
 async function main(){
     let setTimer = 300;
-    let maxDepth = 5;
+    let maxDepth = 3;
     const knownList = [];
     const newURLs = [];
     const options = yargs
         .usage("Usage: -u <url>")
         .option("u",{ alias: "url", describe: "Link/URL to start crawling from", type: "string", demandOption: true })
         .option("t",{ alias: "time", describe: "Sets a timer for when to stop (sec) (default: 300sec) (0 - run forever)", type: "number", demandOption: false})
-        .option("d",{ alias: "depth", describe: "Sets the number of nested links the program will traverse before stopping (default: 5) (0 - run forever)", type: "number", demandOption: false})
+        .option("d",{ alias: "depth", describe: "Sets the number of nested links the program will traverse before stopping (default: 3) (0 - run forever)", type: "number", demandOption: false})
         .argv;
     if(options.depth){
         if(options.depth < 0){
