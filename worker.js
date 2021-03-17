ae54df60efc851e317f5751f5f597bed2ef2233b
Kien Tran <kien.tran@telestream.net>
2021-03-16 17:18:15 -0700

    Bringing it back to worker pools

--- worker.js
+++ worker.js
@@ -2,22 +2,31 @@ const fetch = require("node-fetch");
 const {parseHTML} = require('./tools_utils.js');
 const { parentPort }  = require('worker_threads');
 
-parentPort.on("message", async (message) => {
+function getURL (intial){
+    return new Promise(async(resolve, reject)=>{
+        try{
+            let data, stringifiedData = '';
+            data = await fetch(intial);
+            stringifiedData =  await data.text();
+            resolve(stringifiedData);
+        }
+        catch(err){
+            reject(err);
+        }
+    })
+    
+}
+
+parentPort.on("message", (message) => {
     if(message.exit){
         process.exit(0)
     }
-    let initialURL = message.url;
-    let data, stringifiedData = '';
-    try{
-        data = await fetch(initialURL);
-        stringifiedData =  await data.text();
-    }
-    catch(err){
-        console.error(`Invalid URL provided: ${initialURL}\n${err}`);
-        parentPort.postMessage({currentURL: initialURL, foundURL:[]});
-        process.exit(0);
-    }
-    let arrayURL = parseHTML(stringifiedData, message.url);
-    parentPort.postMessage({currentURL: initialURL, foundURL:arrayURL});
-    process.exit(0);
+    getURL(message.url)
+        .then((data)=>{
+            parentPort.postMessage({currentURL: message.url, foundURL: parseHTML(data, message.url)});
+        })
+        .catch((err)=>{
+            console.error(`Invalid URL provided: ${initialURL}\n${err}`);
+            parentPort.postMessage({currentURL: initialURL, foundURL:[]});
+        })
 });
